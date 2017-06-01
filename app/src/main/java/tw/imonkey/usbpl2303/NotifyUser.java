3af3be8adbc40e5c6ca04289dd1babf8db015d9f
imonkey <i0932702567@gmail.com>
2017-06-02 07:40:14 +0800

    po-po PLC

--- app/src/main/java/tw/imonkey/usbpl2303/NotifyUser.java
+++ app/src/main/java/tw/imonkey/usbpl2303/NotifyUser.java
@@ -1,37 +1,43 @@
 package tw.imonkey.usbpl2303;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
 class NotifyUser {
+
     static void SMSPUSH( String deviceId ,String memberEmail,String message){
-        DatabaseReference mSMSMaster= FirebaseDatabase.getInstance().getReference("/LOG/SMS/");
+        DatabaseReference mSMS= FirebaseDatabase.getInstance().getReference("/LOG/SMS/");
         Map<String, Object> SMS = new HashMap<>();
         SMS.clear();
         SMS.put("message",message);
         SMS.put("deviceId",deviceId);
         SMS.put("memberEmail",memberEmail);
         SMS.put("timeStamp", ServerValue.TIMESTAMP);
-        mSMSMaster.push().setValue(SMS);
+        mSMS.push().setValue(SMS);
+        dataLimit(mSMS,1000);
     }
 
     static void emailPUSH( String deviceId ,String memberEmail,String message ){
-        DatabaseReference mEMAILMaster= FirebaseDatabase.getInstance().getReference("/LOG/EMAIL/");
+        DatabaseReference mEMAIL= FirebaseDatabase.getInstance().getReference("/LOG/EMAIL/");
         Map<String, Object> EMAIL = new HashMap<>();
         EMAIL.clear();
         EMAIL.put("message",message);
         EMAIL.put("deviceId",deviceId);
         EMAIL.put("memberEmail",memberEmail);
         EMAIL.put("timeStamp", ServerValue.TIMESTAMP);
-        mEMAILMaster.push().setValue(EMAIL);
+        mEMAIL.push().setValue(EMAIL);
+        dataLimit(mEMAIL,1000);
 
     }
 
     static void topicsPUSH( String deviceId ,String memberEmail,String message_title,String message_body){
-        DatabaseReference mPUSHMaster= FirebaseDatabase.getInstance().getReference("/LOG/PUSHTopics/");
+        DatabaseReference mTopicsPUSH= FirebaseDatabase.getInstance().getReference("/LOG/PUSHTopics/");
 
         Map<String, Object> message = new HashMap<>();
         message.clear();
@@ -44,11 +50,12 @@ class NotifyUser {
         PUSH.put("deviceId",deviceId);
         PUSH.put("memberEmail",memberEmail);
         PUSH.put("timeStamp", ServerValue.TIMESTAMP);
-        mPUSHMaster.push().setValue(PUSH);
+        mTopicsPUSH.push().setValue(PUSH);
+        dataLimit(mTopicsPUSH,1000);
     }
 
     static void IIDPUSH(String deviceId,String memberEmail,String message_title,String message_body){
-        DatabaseReference mPUSHMaster= FirebaseDatabase.getInstance().getReference("/LOG/PUSHIID/");
+        DatabaseReference mIIDPUSH= FirebaseDatabase.getInstance().getReference("/LOG/PUSHIID/");
 
         Map<String, Object> message = new HashMap<>();
         message.clear();
@@ -61,8 +68,38 @@ class NotifyUser {
         PUSH.put("deviceId",deviceId);
         PUSH.put("memberEmail",memberEmail);
         PUSH.put("timeStamp", ServerValue.TIMESTAMP);
-        mPUSHMaster.push().setValue(PUSH);
+        mIIDPUSH.push().setValue(PUSH);
+        dataLimit(mIIDPUSH,1000);
     }
+
+    private static void dataLimit(final DatabaseReference mData,final int limit) {
+        mData.addListenerForSingleValueEvent(new ValueEventListener() {
+            int dataCount ;
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                dataCount=(int)(snapshot.getChildrenCount());
+                if((dataCount-limit)>0) {
+                    mData.orderByKey().limitToFirst(dataCount - limit)
+                            .addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot snapshot) {
+                                    for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                                        mData.child(childSnapshot.getKey()).removeValue();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                }
+                            });
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+  }
 }
 
 
